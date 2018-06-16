679cc39070d7622c686686064bd8086abbc37de3
JojoWangOk <1059326154@qq.com>
2018-06-16 16:53:21 +0800

    finish

--- PubSub.js
+++ PubSub.js
@@ -13,14 +13,30 @@ module.exports = class PubSub {
 
   subscribe(type, fn) {
     // todo subscribe
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = []
+    }
+    let listeners = this.subscribers[type]
+    listeners.push(fn)
+    console.log(111, listeners)
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    let listeners = this.subscribers[type]
+    if (!listeners) { return }
+    this.subscribers[type] = listeners.filter(listener => listener !== fn);
   }
 
   publish(type, ...args) {
     // todo publish
+    let listeners = this.subscribers[type]
+    if (!listeners) { return }
+    listeners.forEach(item => {
+      item(...args)
+    })
+
+
   }
 
 }
